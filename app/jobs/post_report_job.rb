2e65533f59efb6d61f656ba39d4fcabcdeccee29
Angel Gonzalez <angegonzalez@unal.edu.co>
2020-07-17 13:33:05 -0500

    Creacion del API completo

---
+++ app/jobs/post_report_job.rb
@@ -0,0 +1,11 @@
+class PostReportJob < ApplicationJob
+  queue_as :default
+
+  def perform(user_id, post_id)
+    # user => report post => email with report
+    user = User.find(user_id)
+    post = Post.find(post_id)
+    report = PostReport.generate(post)
+    PostReportMailer.post_report(user, post, report).deliver_now
+  end
+end
